8bbaa46151a7386874efcce8e89003fe4abdf6a1
Misha <popovychmykhailo00@gmail.com>
2021-03-13 12:46:04 +0200

    Added App component to display Title, Input, Tasks components. Added app global styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import Title from './components/Title/Title';
+import InputContainer from './components/Input/InputContainer';
+import TasksContainer from './components/Tasks/TasksContainer';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="content">
+      <Title />
+      <InputContainer />
+      <TasksContainer />
     </div>
   );
-}
+};
 
 export default App;
